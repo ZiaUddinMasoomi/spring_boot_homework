6fad3ca1b0154f6b75def96ce5475cc4b47918f8
ziauddin.massomi <z.massomi@pomtech.com>
2017-10-25 10:47:38 +0430

    the CRUD of my homework it means that I did the CRUD of my part -> department

---
+++ department_crud/src/main/java/homework/department/model/Department.java
@@ -0,0 +1,30 @@
+package homework.department.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "department")
+public class Department {
+
+    @Column(name = "department_id")
+    private Long id;
+    private String name;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
